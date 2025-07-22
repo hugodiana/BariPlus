0a6b452a2db16bd593e0fa9d86dff586d94e4d76
hugodiana <hugo-hdiana@hotmail.com>
2025-07-22 16:40:13 -0300

    Email3

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import './LoginPage.css';
 import Modal from '../components/Modal';
 import { toast } from 'react-toastify';
